5befb873973f24c129dc50d2a9dd98e73da76322
rv602 <rajagopalan602@gmail.com>
2023-07-05 22:20:15 +0530

    <Feature> : [added clipboard listening feature]

--- reciever.py
+++ reciever.py
@@ -3,5 +3,5 @@ import time
 
 if __name__ == "__main__":
     # create a peer in a separate thread
-    peer1 = peer.Peer("10.113.18.210", 5000)
+    peer1 = peer.Peer("10.113.18.56", 5000)
     peer1.start()
--- sender.py
+++ sender.py
@@ -1,11 +1,26 @@
 import peer
 import time
+import clipboard
 
 if __name__ == "__main__":
-    # create a peer in a separate thread
-    peer1 = peer.Peer("10.113.18.210", 6000)
-    peer1.connect("10.113.18.210", 5000)
+    # Create a peer in a separate thread
+    peer1 = peer.Peer("10.113.18.56", 6000)
+    peer1.connect("10.113.18.56", 5000)
+
+    # Store the previous clipboard content
+    previous_clipboard_content = ""
 
     while True:
-        peer1.send_data("Hello from peer1")
-        time.sleep(1)
+        # Get the current clipboard content
+        clipboard_content = clipboard.paste()
+
+        # Check if the clipboard content has changed
+        if clipboard_content != previous_clipboard_content:
+            # Send the clipboard content to the connected peer
+            peer1.send_data(clipboard_content)
+
+            # Update the previous clipboard content
+            previous_clipboard_content = clipboard_content
+
+        # Sleep for a short duration
+        time.sleep(0.5)
